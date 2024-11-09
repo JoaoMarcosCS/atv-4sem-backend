1babc4c0140664e4ebe845fc75aa67b35aef0813
João Marcos <jmcsjoaomarcos@gmail.com>
2024-11-09 00:04:19 +0000

    create CRUD of products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',      
+      port: 5432,             
+      username: 'postgres',   
+      password: 'password',   
+      database: 'my_database',
+      entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+      synchronize: true,         
+    }),
+    ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
